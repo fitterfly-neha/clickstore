2fd1e2d9da15514becd0a79393ae358f4bbfa842
Neha Nagurkar <neha.nagurkar@fitterfly.in>
2022-12-02 08:45:22 +0530

    devise installation in app

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  devise_for :users
+  # get 'welcome/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/index', to: "welcome#index"
 end
